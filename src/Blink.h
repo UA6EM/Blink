0e1253696dc56d18719be9a8e64f4621465365cb
Victor <ua6em@yandex.ru>
2022-01-08 14:43:13 +0300

    Update Blink.h

--- src/Blink.h
+++ src/Blink.h
@@ -10,7 +10,7 @@
 // описание класса
 class Blink {   // Blink
   public:
-    Blink(uint8_t led_pin, uint16_t led_on, uint16_t led_off, uint16_t count, bool inverse=false);
+    Blink(uint8_t led_pin, uint16_t led_on=500, uint16_t led_off=500, uint16_t count=100, bool inverse=false);
     void start();
     void myrun();
   private:
